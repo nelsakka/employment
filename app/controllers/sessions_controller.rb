63d5d9d75907737865b56876e28b04f83216d4bd
Noor <noorel-sakka@hotmail.com>
2016-04-16 14:06:27 +0300

    Added login and logout paths

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+	def new
+	end
+
+	def create
+      user = User.find_by(email:
+          params[:sessions][:email].downcase)
+      if user && user.password ==
+          params[:sessions][:password]
+        log_in user
+        flash[:notice] = 'Logged in'
+        redirect_to root_path
+      else
+        flash.now[:alert] = 'Invalid email/password'
+        render 'new'
+      end 
+    end
+
+    def destroy
+      log_out
+      flash[:notice] = 'Logged out'
+      redirect_to root_path
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,15 @@ Rails.application.routes.draw do
   resources :roles
   resources :users
   root to: 'visitors#index'
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  # post 'employment/'
+  # get 'employment/new' => 'employment#new', as: :login
+  # delete 'employment/delete' => 'employment#destroy', as: :logout
+
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 end
